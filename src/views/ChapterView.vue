484da7f749c9338b1bc509e65d0dbf2f06d4723b
ruei0114 <ray1014000@gmail.com>
2024-03-20 16:52:19 +0800

    start try using https://antoniandre.github.io/splitpanes/#do-your-own-style

--- src/views/ChapterView.vue
+++ src/views/ChapterView.vue
@@ -5,28 +5,20 @@
   <div class="chapter-view">
     <Sidebar />
     <main>
-      <div class="wrapper" ref="box" @mousemove="Dragging" @mouseup="stopDrag">
-        <div
-          class="description"
-          ref="description"
-          :style="{ width: descriptionWidth + 'px' }"
-        >
-          description
+      <div class="wrapper">
+        <splitpanes class="default-theme">
+          <pane>
+            <div>{{ i }}</div>
+          </pane>
+          <pane>
+            <div>{{ i }}</div>
+          </pane>
+        </splitpanes>
+        <!-- <div class="description">description</div>
+        <div class="navigation-bar" @mousedown="onMousedown">
+          <div class="navigation-button"></div>
         </div>
-        <div class="navigation-bar">
-          <div
-            class="navigation-button"
-            ref="navigation"
-            @mousedown.prevent="startDrag"
-          ></div>
-        </div>
-        <div
-          class="code-view"
-          ref="codeView"
-          :style="{ width: codeViewWidth + 'px' }"
-        >
-          code
-        </div>
+        <div class="code-view">code</div> -->
       </div>
     </main>
   </div>
@@ -37,40 +29,82 @@ import Sidebar from "../components/SideBar.vue";
 </script>
 
 <script>
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
+
 export default {
-  data() {
-    return {
-      isDragging: false,
-      startX: 0,
-      left: 0
-    };
-  },
-  methods: {
-    startDrag(event) {
-      this.isDragging = true;
-      this.startX = event.clientX;
-      this.left = this.$refs.navigation.offsetLeft;
-    },
-    dragging(event) {
-      if (this.isDragging) {
-        var endX = event.clientX;
-        var moveLen = this.left+ (endX - this.startX);
-        var maxT = this.$refs.box.clientWidth - this.$refs.navigation.offsetWidth;
-
-        if (moveLen < 32) moveLen = 32; // 左边区域的最小宽度为32px
-        if (moveLen > maxT - 150) moveLen = maxT - 150; //右边区域最小宽度为150px
-
-        this.$refs.navigation.style.left = moveLen + "px";
-      }
-    },
-    stopDrag() {
-      this.isDragging = false;
-    },
-  },
+  components: { Splitpanes, Pane },
 };
+// export default {
+//   data() {
+//     return {
+//       isDragging: false,
+//       x: 0,
+//       y: 0,
+//       resizer: document.querySelector(".navigation-bar"),
+//       leftSide: resizer.previousElementSibling,
+//       reghtSide: resizer.nextElementSibling,
+//       leftWidth: 0,
+//     };
+//   },
+//   methods: {
+//     onMousedown(e) {
+//       this.isDragging = true;
+//       console.log("mousedown");
+//       // Get the current mouse position
+
+//       this.resizer = e.target;
+//       this.leftSide = resizer.previousElementSibling;
+//       this.rightSide = resizer.nextElementSibling;
+
+//       this.x = e.clientX;
+//       this.leftWidth = this.leftSide.getBoundingClientRect().width;
+//       window.addEventListener("mousemove", this.onMousemove);
+//       window.addEventListener("mouseup", this.onMouseup);
+//     },
+//     onMousemove(e) {
+//       if (this.isDragging) {
+//         console.log("dragging");
+//         // How far the mouse has been moved
+//         const dx = e.clientX - this.x;
+
+//         const newLeftWidth =
+//           ((this.leftWidth + dx) * 100) /
+//           resizer.parentNode.getBoundingClientRect().width;
+//         leftSide.style.width = newLeftWidth + "%";
+//       }
+//     },
+//     onMouseup() {
+//       this.isDragging = false;
+//       window.removeEventListener("mousemove", this.onMousemove);
+//       window.removeEventListener("mouseup", this.onMouseup);
+//     },
+//   },
+
+// };
 </script>
 
 <style scoped>
+.splitpanes {
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+}
+
+.splitpanes__pane {
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2) inset;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+.splitpanes--vertical > .splitpanes__splitter {
+  min-width: 6px;
+  background: linear-gradient(90deg, #ccc, #111);
+}
+
+.splitpanes--horizontal > .splitpanes__splitter {
+  min-height: 6px;
+  background: linear-gradient(0deg, #ccc, #111);
+}
 .chapter-view {
   display: flex;
   overflow-wrap: break-word;
@@ -88,6 +122,11 @@ export default {
       height: 97vh;
       width: 100%;
 
+      /* .splitpanes--horizontal > .splitpanes__splitter {
+        min-height: 6px;
+        background: linear-gradient(0deg, #ccc, #111);
+      } */
+
       .description {
         background-color: #2f2f2f;
         margin-right: 4px;
@@ -100,6 +139,8 @@ export default {
       .navigation-bar {
         display: flex;
         align-items: center;
+        cursor: ew-resize;
+
         .navigation-button {
           background-color: #2f2f2f;
           height: 2rem;
