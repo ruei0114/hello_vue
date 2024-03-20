9117eb9c76194387c7b4fb02e59b3eaea2240a01
ruei0114 <ray1014000@gmail.com>
2024-03-20 13:21:16 +0800

    make splitter but failed

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,105 +1,120 @@
 <template>
-    <aside :class="`${is_expanded && 'is-expanded'}`">
-        <div class="catalog"></div>
-        <div class="menu-toggle-wrap">
-            <button class="menu-toggle" @click="toggleMenu">
-                <span class="material-symbols-outlined">keyboard_double_arrow_right</span>
-            </button>
-        </div>
-        
-    </aside>
+  <aside :class="`${is_expanded && 'is-expanded'}`">
+    <div class="chapter-container">
+      <div
+        v-for="(chapterStatus, index) in chapterStatus"
+        :key="index"
+        class="chapter-box"
+        :class="{
+          completed: chapterStatus === 'completed',
+          'in-progress': chapterStatus === 'in-progress',
+          'not-started': chapterStatus === 'not-started',
+        }"
+        @click="handleChapterClick(index)"
+      >
+        chapter {{ index }}
+      </div>
+    </div>
+    <div class="menu-toggle-wrap">
+      <button class="menu-toggle" @click="toggleMenu">
+        <span class="material-symbols-outlined"
+          >keyboard_double_arrow_right</span
+        >
+      </button>
+    </div>
+  </aside>
 </template>
 
 <script setup>
-    import { ref } from 'vue';
-    const is_expanded = ref(false);
-    const toggleMenu = () => {
-        is_expanded.value = !is_expanded.value;
-    };
+import { ref } from "vue";
+const is_expanded = ref(false);
+const toggleMenu = () => {
+  is_expanded.value = !is_expanded.value;
+};
 </script>
 
 <style lang="scss" scoped>
-    aside {
-        display: flex;
-        justify-content: end;
-        min-height: 100vh;
-        // #bfb7b7
-        background-color: transparent;
-        width: 0;
-        overflow: hidden;
-        position: fixed;
-        padding: 1rem;
-        transition: 0.2s ease-out;
+aside {
+  display: flex;
+  justify-content: end;
+  min-height: 100vh;
+  // #bfb7b7
+  background-color: transparent;
+  width: 0;
+  overflow: hidden;
+  position: fixed;
+  padding: 1rem;
+  transition: 0.2s ease-out;
 
-        .catalog {
-            width: 0;
-            transition: 0.2s ease-out;
+  .chapter-container {
+    width: 0;
+    transition: 0.2s ease-out;
+  }
+
+  .menu-toggle-wrap {
+    display: flex;
+    margin-bottom: 1rem;
+    margin-top: -20px;
+    margin-right: -15px;
+
+    position: relative;
+    top: 0;
+    transition: 0.2s ease-out;
+
+    .menu-toggle {
+      transition: 0.2s ease-out;
+      font-size: 2rem;
+      background-color: transparent;
+      color: #888489;
+      border: none;
+
+      .material-symbols-outlined {
+        font-size: 1.5rem;
+        cursor: pointer;
+      }
+
+      &:hover {
+        .material-symbols-outlined {
+          color: #bfb7b7;
+          transform: translatex(0.1rem);
         }
-
-        .menu-toggle-wrap {
-            display: flex;
-            margin-bottom: 1rem;
-            margin-top: -20px;
-            margin-right: -15px;
-
-            position: relative;
-            top: 0;
-            transition: 0.2s ease-out;
-
-            .menu-toggle {
-                transition: 0.2s ease-out;
-                font-size: 2rem;
-                background-color: transparent;
-                color: #888489;
-                border: none;
-
-                .material-symbols-outlined {
-                    font-size: 1.5rem;
-                    cursor: pointer;
-                }
-
-                &:hover {
-                    .material-symbols-outlined {
-                        color: #bfb7b7;
-                        transform: translatex(0.1rem);
-                    }
-                }
-            }
-        }
-
-        &.is-expanded {
-            width: 300px;
-
-            .catalog {
-                margin-top: -20rem;
-                margin-left: -20rem;
-                width: 16.5rem;
-                padding: 1rem;
-                background-color: #bfb7b7;
-                border-radius: 0.5rem;
-                box-shadow: 0 0 10px 0 #000000;
-            }
-            
-            .menu-toggle-wrap{
-                justify-content: flex-end;
-                .menu-toggle {
-                    .material-symbols-outlined {
-                        transform: rotate(-180deg);
-                    }
-                }
-
-                &:hover {
-                    .material-symbols-outlined {
-                        color: #bfb7b7;
-                        transform: rotate(-180deg) translatex(0.1rem);
-                    }
-                }
-            }
-        }
-
-        @media (max-width: 768px) {
-            position: fixed;
-            z-index: 99;
-        }
+      }
     }
+  }
+
+  &.is-expanded {
+    width: 300px;
+
+    .chapter-container {
+      margin-top: -20rem;
+      margin-left: -20rem;
+      width: 16.5rem;
+      padding: 1rem;
+      background-color: #bfb7b7;
+      border-radius: 0.5rem;
+      box-shadow: 0 0 10px 0 #000000;
+    }
+
+    .menu-toggle-wrap {
+      justify-content: flex-end;
+      .menu-toggle {
+        .material-symbols-outlined {
+          transform: rotate(-180deg);
+        }
+      }
+
+      &:hover {
+        .material-symbols-outlined {
+          color: #bfb7b7;
+          transform: rotate(-180deg) translatex(0.1rem);
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+  }
+}
 </style>
--- src/views/ChapterView.vue
+++ src/views/ChapterView.vue
@@ -4,26 +4,127 @@
 <template>
   <div class="chapter-view">
     <Sidebar />
-    <main>chappppppppppppppppppppppppppppppppppppppppppppppppppppp</main>
+    <main>
+      <div class="wrapper" ref="box" @mousemove="Dragging" @mouseup="stopDrag">
+        <div
+          class="description"
+          ref="description"
+          :style="{ width: descriptionWidth + 'px' }"
+        >
+          description
+        </div>
+        <div class="navigation-bar">
+          <div
+            class="navigation-button"
+            ref="navigation"
+            @mousedown.prevent="startDrag"
+          ></div>
+        </div>
+        <div
+          class="code-view"
+          ref="codeView"
+          :style="{ width: codeViewWidth + 'px' }"
+        >
+          code
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script setup>
- import Sidebar from "../components/SideBar.vue";
+import Sidebar from "../components/SideBar.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      isDragging: false,
+      startX: 0,
+      left: 0
+    };
+  },
+  methods: {
+    startDrag(event) {
+      this.isDragging = true;
+      this.startX = event.clientX;
+      this.left = this.$refs.navigation.offsetLeft;
+    },
+    dragging(event) {
+      if (this.isDragging) {
+        var endX = event.clientX;
+        var moveLen = this.left+ (endX - this.startX);
+        var maxT = this.$refs.box.clientWidth - this.$refs.navigation.offsetWidth;
+
+        if (moveLen < 32) moveLen = 32; // 左边区域的最小宽度为32px
+        if (moveLen > maxT - 150) moveLen = maxT - 150; //右边区域最小宽度为150px
+
+        this.$refs.navigation.style.left = moveLen + "px";
+      }
+    },
+    stopDrag() {
+      this.isDragging = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
- .chapter-view {
-   display: flex;
-   main{
+.chapter-view {
+  display: flex;
+  overflow-wrap: break-word;
+
+  main {
+    display: flex;
     flex: 1 1 0;
-    margin-left: 2rem;
-    padding :2rem;
+    margin-left: 50px;
+    height: 100vh;
     color: bisque;
+    align-items: center;
+
+    .wrapper {
+      display: flex;
+      height: 97vh;
+      width: 100%;
+
+      .description {
+        background-color: #2f2f2f;
+        margin-right: 4px;
+        border-radius: 5px;
+        width: 50%;
+        padding: 2rem;
+        color: bisque;
+      }
+
+      .navigation-bar {
+        display: flex;
+        align-items: center;
+        .navigation-button {
+          background-color: #2f2f2f;
+          height: 2rem;
+          width: 5px;
+          border-radius: 3px;
+          cursor: ew-resize;
+
+          /* &:mousedown {
+          } */
+        }
+      }
+
+      .code-view {
+        background-color: #2f2f2f;
+        margin: 0px 10px 0px 4px;
+        border-radius: 5px;
+        width: 50%;
+        padding: 2rem;
+        color: bisque;
+      }
+    }
 
     @media (max-width: 768px) {
       padding-left: 6rem;
     }
-   }
- }
+  }
+}
 </style>
