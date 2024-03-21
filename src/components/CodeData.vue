c29287b8eed4c23207edb4338df2e73e3f2b92dd
ruei0114 <ray1014000@gmail.com>
2024-03-21 22:38:14 +0800

    scroll bar style & resizeObserver Error resolved

--- src/components/CodeData.vue
+++ src/components/CodeData.vue
@@ -1,40 +1,50 @@
 <template>
     <div>
-        <CodeEditor
-            width="auto"
-            theme="github-dark"
-            :tab-spaces="4"
-            :wrap="true"
-        />
+        <CodeEditor width="100%" />
     </div>
 </template>
 
 <script>
 // import hljs from "highlight.js";
 import CodeEditor from "simple-code-editor";
-// import { ref, watch } from "vue";
-// import debounce from "lodash.debounce";
 export default {
     components: {
         CodeEditor,
         // hljs,
     },
-    // setup() {
-    //     const value = ref("");
-    //     const debouncedValue = ref("");
-
-    //     const debouncedUpdate = debounce((newValue) => {
-    //         debouncedValue.value = newValue;
-    //     }, 500);
-
-    //     watch(value, (newValue) => {
-    //         debouncedUpdate(newValue);
-    //     });
-
-    //     return {
-    //         value,
-    //         debouncedValue,
-    //     };
-    // },
 };
 </script>
+
+<style>
+.code-editor .code-area > pre {
+    max-height: 660px;
+    max-width: 850px;
+}
+.code-editor .code-area > textarea {
+    /* background-color: aqua; */
+    /* color: black; */
+
+    max-height: 660px;
+    max-width: 850vh;
+    overflow: scroll;
+}
+
+/* https://css-tricks.com/almanac/properties/s/scrollbar/ */
+.code-editor .code-area > textarea::-webkit-scrollbar {
+    width: 12px;
+    height: 12px;
+    cursor: default;
+}
+.code-editor .code-area > textarea::-webkit-scrollbar-track {
+    cursor: default;
+    border-radius: 5px;
+}
+.code-editor .code-area > textarea::-webkit-scrollbar-thumb {
+    background-color: #202020;
+    cursor: default;
+    border-radius: 5px;
+}
+.code-editor .code-area > textarea::-webkit-scrollbar-corner {
+    background-color: #0d1117;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,3 +3,25 @@ import App from './App.vue'
 import {router} from './router'
 
 createApp(App).use(router).mount('#app')
+
+
+// https://blog.csdn.net/qq_45112637/article/details/131740110 解决resizeObserver Error
+const debounce = (fn, delay) => {
+  let timer = null;
+  return function() {
+    let context = this;
+    let args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
+
+const _ResizeObserver = window.ResizeObserver;
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+    constructor(callback) {
+        callback = debounce(callback, 16);
+        super(callback);
+    }
+}
